daf4940a63220976f9263ca012898dc6ec2e4c85
Behrouz Hosseini <bhoss@nets.eu>
2021-07-29 12:04:02 +0200

    Make the console application to get response from API with Authentication

--- src/SecureClient/Program.cs
+++ src/SecureClient/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
 
 namespace SecureClient
 {
@@ -6,7 +11,68 @@ namespace SecureClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Making call ...");
+            RunAsyns().GetAwaiter().GetResult();
+        }
+
+        private static async Task RunAsyns(){
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json");
+
+            IConfidentialClientApplication app;
+
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+            .WithClientSecret(config.ClientSecret)
+            .WithAuthority(new Uri(config.Authority))
+            .Build();
+
+            string[] ResourceIds = new string[] {config.ResourceId};
+
+            AuthenticationResult result = null;
+
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token Acquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+            }
+            
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+
+                if(defaultRequestHeaders.Accept.All(m => m.MediaType != "application/json"))
+                {
+                   httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                }
+
+                defaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine($"Failed to call API: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
         }
     }
 }
